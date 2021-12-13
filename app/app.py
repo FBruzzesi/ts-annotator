9b27c2dde407cdcdc1a53cf2a56aab67b0a39241
FBruzzesi <fbruzzesi@libero.it>
2021-12-13 18:42:31 +0100

    procfile

--- app/app.py
+++ app/app.py
@@ -3,12 +3,12 @@ import dash_bootstrap_components as dbc
 from flask import Flask
 
 # Flask server
-server = Flask(__name__)
+application = Flask(__name__)
 
 # Dash app
 app = dash.Dash(
     __name__,
-    server=server,
+    server=application,
     suppress_callback_exceptions=True,
     title='ts-annotator',
     external_stylesheets=[
